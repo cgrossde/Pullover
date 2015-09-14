30fd2d1b1751b36762b8ee90007b600fa35765f2
Christoph Gross <gross@blubyte.de>
2015-09-14 18:20:27 +0200

    Tested react-bootstrap on about page

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
+import { Row, Col, Table } from 'react-bootstrap'
 
 export default class About extends Component {
   displayName: 'About'
   render() {
     return (
-      <div>
-        <h1>About</h1>
-        <p>About Pullover</p>
-      </div>
+      <Row>
+        <Col md={8} mdOffset={2}>
+          <h1 className="center-block">About Pullover</h1>
+          <span>The unofficial Pushover desktop client</span>
+          <Row>
+            <Col xs={10} xsOffset={1}>
+              <Table>
+                <tbody>
+                  <tr>
+                    <th>Version</th>
+                    <td className="info-version"></td>
+                  </tr>
+                  <tr>
+                    <th>Homepage</th>
+                    <td><a className="github-link" href="#">Github cgrossde/pullover</a></td>
+                  </tr>
+                  <tr>
+                    <th>Found a bug?</th>
+                    <td><a className="github-issue-link" href="#">Report it here</a></td>
+                  </tr>
+                  <tr>
+                    <th>Messages received</th>
+                    <td className="messages-received">none</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     )
   }
 }
--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
 
+import './App.scss'
+
 export default class App extends Component {
   displayName: 'App'
   render() {
     return (
       <div>
         <Navbar />
-        {this.props.children}
+        <div className="appContainer">
+          {this.props.children}
+        </div>
       </div>
     )
   }
