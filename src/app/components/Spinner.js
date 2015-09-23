b7a5f4999e677feea31d1a70e0bdd8774de6e260
Christoph Gross <gross@blubyte.de>
2015-09-23 10:38:55 +0200

    Add spinner component for loading overlays

---
+++ src/app/components/Spinner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Spinner.scss'
+
+const Spinner = React.createClass({
+  displayName: 'Spinner',
+
+  render() {
+    var spinner = (
+      <div className="spinner">
+          <img className="spinner-image" src="images/spinner.gif" alt="Loading..."/>
+        </div>
+    )
+    return (this.props.active) ? spinner : null
+  }
+})
+
+export default Spinner
