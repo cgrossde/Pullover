e023bf41d76b66a7ea3e2fa4f80d7ce2f9159857
Christoph Gross <gross@blubyte.de>
2016-02-03 10:12:32 +0100

    Remove redux settings store

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -2,13 +2,10 @@ import { combineReducers } from 'redux'
 import { routerStateReducer } from 'redux-router'
 
 import { pushoverStateReducer } from './PushoverStateReducer'
-import { settingsStateReducer } from './SettingsStateReducer'
 
-console.log('Init store')
 const reducer = combineReducers({
 	router: routerStateReducer,
   pushover: pushoverStateReducer,
-  settings: settingsStateReducer
 })
 
 export default reducer
