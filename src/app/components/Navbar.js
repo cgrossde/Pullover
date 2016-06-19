e4591ba56ab915a0f65f7fb6644c192bf702be3b
Christoph Gross <gross@blubyte.de>
2016-06-19 22:07:53 +0200

    Drag fix nwjs/nw.js#2375 for NavBar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router'
-import { connect } from 'react-redux'
-import { hideWindow } from '../nw/Window'
+import {Link} from 'react-router'
+import {connect} from 'react-redux'
+import {hideWindow} from '../nw/Window'
 
 import './Navbar.scss'
 
@@ -11,21 +11,26 @@ const Navbar = React.createClass({
   render() {
     const icon = (this.props.status === 'ONLINE') ? 'signal' : 'flash'
     const iconClass = 'glyphicon glyphicon-' + icon
+    // Drag fix (top-fix + bottom-fix): https://github.com/nwjs/nw.js/issues/2375#issuecomment-73217446
     return (
-      <div className="titlebar">
-        <img className="logo" src="images/logo.png"/>
-        <div className="close-button" data-toggle="tooltip" data-placement="bottom" title="Send to tray">
-          <span className="glyphicon glyphicon-remove" onClick={hideWindow}></span>
+      <div>
+        <div id="top-fix"></div>
+        <div className="titlebar">
+          <img className="logo" src="images/logo.png"/>
+          <div className="close-button" data-toggle="tooltip" data-placement="bottom" title="Send to tray">
+            <span className="glyphicon glyphicon-remove" onClick={hideWindow}></span>
+          </div>
+          <div className="settings-button">
+            <Link activeClassName="active" to="/settings"><span className="glyphicon glyphicon-cog"></span></Link>
+          </div>
+          <div className="about-button">
+            <Link activeClassName="active" to="/about"><span className="glyphicon glyphicon-info-sign"></span></Link>
+          </div>
+          <div className="status-button">
+            <Link activeClassName="active" to="/status"><span className={iconClass}></span></Link>
+          </div>
         </div>
-        <div className="settings-button">
-          <Link activeClassName="active" to="/settings"><span className="glyphicon glyphicon-cog"></span></Link>
-        </div>
-        <div className="about-button">
-          <Link activeClassName="active" to="/about"><span className="glyphicon glyphicon-info-sign"></span></Link>
-        </div>
-        <div className="status-button">
-          <Link activeClassName="active" to="/status"><span className={iconClass}></span></Link>
-        </div>
+        <div id="bottom-fix"></div>
       </div>
     )
   }
