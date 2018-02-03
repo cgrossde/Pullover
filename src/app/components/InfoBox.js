e6b9f8d63af09ce7498342d580feffa4ab8fd8c7
Christoph Gross <gross@gross-it.com>
2018-02-03 23:10:54 +0100

    Close Infobox when clicking outside of content area.

--- src/app/components/InfoBox.js
+++ src/app/components/InfoBox.js
@@ -3,15 +3,22 @@ import React from 'react'
 import './InfoBox.scss'
 
 class InfoBox extends React.Component {
-  static defaultProps = {
-    title: '',
-    active: false
+  constructor() {
+    super()
+    this.triggerCloseIfClickedOutsideOfContent = this.triggerCloseIfClickedOutsideOfContent.bind(this)
+  }
+
+  static get defaultProps() {
+    return {
+      title: '',
+      active: false
+    }
   }
 
   render() {
     const title = (this.props.title) ? (<h2>{this.props.title}</h2>) : ''
     const infobox = (
-      <div className="infobox">
+      <div className="infobox" onClick={this.triggerCloseIfClickedOutsideOfContent}>
         <div className="infobox-content">
           <span className="infobox-close glyphicon glyphicon-remove" onClick={this.props.close}/>
           {title}
@@ -20,6 +27,12 @@ class InfoBox extends React.Component {
       </div>
     )
     return (this.props.active) ? infobox : null
+  }
+
+  triggerCloseIfClickedOutsideOfContent(event) {
+    if (event.target === event.currentTarget) {
+      this.props.close(event)
+    }
   }
 }
 
